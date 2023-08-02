eb19aa959caaeea91cdb5c55ba5c3c761ce66c82
yavuzyarkinokular <yarkinokular@gmail.com>
2023-08-02 11:39:35 +0300

    Geçici bir veritabanı oluşturuldu key-value ilişkisi ile

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,33 @@ const router = express.Router();
 /* ---- Import Bitiş ---   */
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Ana Sayfa" });
+  const products = [
+    {
+      name: "iPhone 11",
+      price: 50225,
+      image: "1.jpeg",
+      description: " Ekranı en büyük telefon ",
+    },
+    {
+      name: "iPhone 12",
+      price: 50225,
+      image: "2.jpeg",
+      description: " Ekranı en büyük telefon ",
+    },
+    {
+      name: "iPhone 13",
+      price: 50225,
+      image: "3.jpeg",
+      description: " Ekranı en büyük telefon ",
+    },
+    {
+      name: "iPhone 14",
+      price: 50225,
+      image: "4.jpeg",
+      description: " Ekranı en büyük telefon ",
+    },
+  ];
+  res.render("index", { title: "Ana Sayfa", products: products });
 });
 
 module.exports = router;
