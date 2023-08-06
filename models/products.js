349e3f6678cbc6aaa01c7793fd05469f5c470134
yavuzyarkinokular <yarkinokular@gmail.com>
2023-08-06 09:50:39 +0300

    6_6 Urun Listesi

--- models/products.js
+++ models/products.js
@@ -1,5 +1,29 @@
-const products = [];
-
+const products = [
+  {
+    name: "samsung s8",
+    price: "5500",
+    imageUrl: "1.jpeg",
+    description: "Kıyak telefon",
+  },
+  {
+    name: "samsung s9",
+    price: "5750",
+    imageUrl: "1.jpeg",
+    description: "Kıyak telefon",
+  },
+  {
+    name: "samsung s13",
+    price: "7000",
+    imageUrl: "1.jpeg",
+    description: "Kıyak telefon",
+  },
+  {
+    name: "samsung s23 ultra",
+    price: "13000",
+    imageUrl: "1.jpeg",
+    description: "Kıyak telefon",
+  },
+];
 module.exports = class Product {
   constructor(name, price, imgUrl, description) {
     this.name = name;
