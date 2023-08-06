026ad921f866cd75b8721a69a6e124b5a96f6d3e
yavuzyarkinokular <yarkinokular@gmail.com>
2023-08-06 10:05:45 +0300

    6_7 Random Urun Id

--- models/products.js
+++ models/products.js
@@ -1,23 +1,27 @@
 const products = [
   {
+    id: "12345",
     name: "samsung s8",
     price: "5500",
     imageUrl: "1.jpeg",
     description: "Kıyak telefon",
   },
   {
+    id: "12346",
     name: "samsung s9",
     price: "5750",
     imageUrl: "1.jpeg",
     description: "Kıyak telefon",
   },
   {
+    id: "12349",
     name: "samsung s13",
     price: "7000",
     imageUrl: "1.jpeg",
     description: "Kıyak telefon",
   },
   {
+    id: "12340",
     name: "samsung s23 ultra",
     price: "13000",
     imageUrl: "1.jpeg",
@@ -26,6 +30,7 @@ const products = [
 ];
 module.exports = class Product {
   constructor(name, price, imgUrl, description) {
+    this.id = Math.floor(Math.random() * 99999) + 1;
     this.name = name;
     this.price = price;
     this.imageUrl = imageUrl;
